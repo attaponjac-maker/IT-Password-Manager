e09e0615675e9bc75e6140b8fb96261935f479fb
J-attapon <attapon.jac@thaimooc.ac.th>
2026-01-21 14:52:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const SUPABASE_CONFIG = {
-    url: 'https://abcdefghijklmnop.supabase.co',
+    url: 'https://celfxsflqfbxxhcndaio.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNlbGZ4c2ZscWZieHhoY25kYWlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5MTczNjUsImV4cCI6MjA4NDQ5MzM2NX0.tFiJcElcVyg1_lPomt03enLnLwH2ZKQZU0iObO4jRm4'
 };
